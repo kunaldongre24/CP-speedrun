5d69df7491e3a37ff9fc8158f1ec75a12291b672
Kunal Dongre <dongrekunal16@gmail.com>
2024-06-22 01:00:21 +0530

    update

---
+++ Dynamic_Programming/fibonacci.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+	int n;cin>>n;
+	unordered_map<int, int> fib;
+	int f;
+	for(int i=1;i<=n;i++){		
+    if(i<=2) f=1;
+	else f=fib[i-1]+fib[i-2];
+	fib[i]=f;
+	}
+	cout<<f<<endl;
+}
